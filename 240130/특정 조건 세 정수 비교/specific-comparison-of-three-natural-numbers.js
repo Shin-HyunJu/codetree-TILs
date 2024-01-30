745e7a9515bc36743c213456621ca94fa88edbfb
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-01-30 17:14:00 +0900

    Title: 특정 조건 세 정수 비교, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240130/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240130/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-let [list] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let list = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 let answer1, answer2;
 
@@ -10,7 +10,7 @@ if(list[0] === Math.min(list)) {
     answer1 = 0;
 }
 
-if(a === b && a === c && b === c) {
+if(list[0]===list[1] && list[0]===list[2] && list[1]===list[2]) {
     answer2 = 1;
 } else {
     answer2 = 0;
