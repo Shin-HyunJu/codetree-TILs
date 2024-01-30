866b201fd9ab9c901fa69465172f02bc2c122a78
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-01-30 17:19:30 +0900

    Title: 특정 조건 세 정수 비교, Time: 226ms, Memory: 14MB, Status: Passed - CodeTree

--- 240130/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240130/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -10,7 +10,7 @@ if(list[0] === Math.min(...list)) {
     answer1 = 0;
 }
 
-if(list[0]===list[1] && list[0]===list[2] && list[1]===list[2]) {
+if(list[0]===list[1] && list[1]===list[2]) {
     answer2 = 1;
 } else {
     answer2 = 0;
