dbadcbd4bdd9e469b6991f4a6fc9fccc69cc9832
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-01-31 11:26:06 +0900

    Title: 남녀노소 구분짓기, Time: 242ms, Memory: 14MB, Status: Passed - CodeTree

--- 240131/남녀노소 구분짓기/sex-and-age.js
+++ 240131/남녀노소 구분짓기/sex-and-age.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let [gender, age] = fs.readFileSync(0).toString().trim().split("\n").map(Number);
 
 if (gender===0) {
-    console.log(age>19 ? 'MAN':'BOY');
+    console.log(age>=19 ? 'MAN':'BOY');
 }else {
-    console.log(age>19 ? 'WOMAN':'GIRL');
+    console.log(age>=19 ? 'WOMAN':'GIRL');
 }
