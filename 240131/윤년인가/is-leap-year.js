a21aadfa7979c94883d19dc319cb8b98773d830f
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-01-31 11:36:17 +0900

    Title: 윤년인가, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

--- 240131/윤년인가/is-leap-year.js
+++ 240131/윤년인가/is-leap-year.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let y = Number(fs.readFileSync(0).toString().trim());
 
-if ((y%4===0 && y%100===0 && y%400===0) || (y%4===0 && y%100!==0)) {
+if (y%400===0 || (y%4===0 && y%100!==0)) {
     console.log('true');
 }else {
     console.log('false');
