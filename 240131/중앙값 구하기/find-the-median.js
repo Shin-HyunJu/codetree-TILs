cc09dca0ad881fabb3c7f69af02815baa81e2a5b
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-01-31 12:21:14 +0900

    Title: 중앙값 구하기, Time: 247ms, Memory: 14MB, Status: Passed - CodeTree

--- 240131/중앙값 구하기/find-the-median.js
+++ 240131/중앙값 구하기/find-the-median.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-input.sort();
+input.sort((a,b)=>{
+    return a-b;
+});
 
 console.log(input[1]);
