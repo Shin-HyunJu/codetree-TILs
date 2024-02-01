323e5873ed9977401574c5400d4f428c89a19290
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-01 11:51:38 +0900

    Title: 홀수만 출력 2, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240201/홀수만 출력 2/output-only-odd-2.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let [b,a ] = fs.readFileSync(0).toString().split(" ").map(Number);
+let result="";
+
+for(let i=b; i>=a; i-=2) {
+    result += i+" ";
+}
+
+console.log(result);
