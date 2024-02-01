29e5eca87c6d73daee3aa36e065725f6b8a15409
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-01 15:08:51 +0900

    Title: a 부터 b 까지, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240201/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let [a,b] = fs.readFileSync(0).toString().split(" ").map(Number);
+
+let result = '';
+
+while(a<=b) {
+    result += a+" ";
+
+    if (a%2!==0) {
+        a *= 2;
+    } else {
+        a += 3;
+    }
+}
+
+console.log(result);
