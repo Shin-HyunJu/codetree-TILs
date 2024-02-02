b6b836d03e136ba1849fa74d184d62996179151f
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-02 15:06:29 +0900

    Title: 1부터의 합, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 240202/1부터의 합/sum-from-the-one.js
+++ 240202/1부터의 합/sum-from-the-one.js
@@ -3,7 +3,7 @@ const n = Number(fs.readFileSync(0, 'utf-8').toString().trim());
 
 let sum = 0;
 
-for (let i=1; i<=100; i++) {
+for (let i=1; i<=5000; i++) {
     if (sum >= n) {
         console.log(i-1);
         break;
