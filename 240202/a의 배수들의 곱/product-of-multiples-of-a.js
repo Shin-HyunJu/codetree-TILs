d07e71f75cfcd556410460f30d2860f056f2e153
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-02 13:51:54 +0900

    Title: a의 배수들의 곱, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240202/a의 배수들의 곱/product-of-multiples-of-a.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const [a, b] = fs.readFileSync(0, 'utf-8').toString().trim().split(" ").map(Number);
+let prod = 1;
+
+for (let i=1; i<=b; i++) {
+    if(i%a === 0){
+        prod *= i;
+    }
+}
+
+console.log(prod)
