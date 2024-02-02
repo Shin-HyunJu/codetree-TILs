bc3c6a895d382f5e84c13eff9f367dfea90eac1c
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-02 15:22:35 +0900

    Title: 1까지 나누기, Time: 171ms, Memory: 15MB, Status: Passed - CodeTree

--- 240202/1까지 나누기/divide-by-1.js
+++ 240202/1까지 나누기/divide-by-1.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0, 'utf-8').toString().trim());
 
 for (let i=1; i<=100; i++) {
-    n /= i;
+    n = parseInt(n/i);
     if(n<=1){
         console.log(i);
         break;
