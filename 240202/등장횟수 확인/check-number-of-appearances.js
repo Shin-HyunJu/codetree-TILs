aa53a43faaebfa253fd7834b0145344e83f83f8b
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-02 11:57:23 +0900

    Title: 등장횟수 확인, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240202/등장횟수 확인/check-number-of-appearances.js
+++ 240202/등장횟수 확인/check-number-of-appearances.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().split('\n').map(Number);
 let cnt = 0;
 
 input.forEach(item => {
-    if(item%2===0) cnt++;
+    if(item%2===0) {cnt++};
 })
 
 console.log(cnt);
