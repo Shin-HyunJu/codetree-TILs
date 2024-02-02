23892066eb8f64e9e2bcaf1114b58e4382d2a55a
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-02 13:46:16 +0900

    Title: 총합 구하기, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240202/총합 구하기/find-the-total.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const [a,b] = fs.readFileSync(0, 'utf-8').toString().trim().split(" ").map(Number);
+let sum = 0;
+
+for (let i=a; i<=b; i++) {
+    if(i%6===0 && i%8!==0) {
+        sum += i;
+    }
+}
+
+console.log(sum);
