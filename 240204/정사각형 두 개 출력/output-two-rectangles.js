2f194173674b3ce02e6cbdcf2625eb3e9b657ecf
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-04 15:21:30 +0900

    Title: 정사각형 두 개 출력, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240204/정사각형 두 개 출력/output-two-rectangles.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let result="";
+let str = "";
+
+for (let i=0; i<n; i++) {
+    str = "";
+    for (let j=0; j<n; j++) {
+        str += '*';
+    }
+    result += str + "\n";
+}
+
+console.log(result);
+console.log(result);
