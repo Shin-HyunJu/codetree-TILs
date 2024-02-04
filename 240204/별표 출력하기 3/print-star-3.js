0e09ddf5af5eac9ff40296ff8bf20cac62aac5bc
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-04 18:25:55 +0900

    Title: 별표 출력하기 3, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240204/별표 출력하기 3/print-star-3.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = "";
+
+for (let i=0; i<n; i++) {
+    str = '';
+    for (let j=0; j<=i-1; j++) {
+        str += "  ";
+    }
+    for (let j=0; j<(2*n)-(2*i)-1; j++) {
+        str +='*'+' '
+    }
+    console.log(str);
+}
