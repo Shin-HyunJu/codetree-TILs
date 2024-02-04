c345c05221e65ded6d1c86fd8b81b875f7d762a0
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-04 13:54:13 +0900

    Title: ab 사이에 있는 c, Time: 231ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240204/ab 사이에 있는 c/c-between-a-and-b.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const [a,b,c] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let satisfied = false;
+
+for(let i=a; i<=b; i++) {
+    if (i % c===0) {
+        satisfied = true;
+        break;
+    }
+}
+
+if (satisfied) {
+    console.log('YES');
+} else {
+    console.log('NO');
+}
