6f8056e6ead19f40b3908a2d299b36d4ddb64053
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-04 14:13:45 +0900

    Title: ab 사이에 없는 c, Time: 322ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240204/ab 사이에 없는 c/c-between-a-and-b-2.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const [a,b,c] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let satisfied = true;
+
+for (let i=a; i<=b; i++) {
+    if (i % c===0) {
+        satisfied = false;
+        break;
+    }
+}
+
+if (satisfied) {
+    console.log('YES');
+} else {
+    console.log('NO');
+}
