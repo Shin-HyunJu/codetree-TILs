177d5ba1b22c14db37beaac0cd9d4ce034bac490
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-04 15:16:08 +0900

    Title: 직사각형 별표 출력하기, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240204/직사각형 별표 출력하기/print-stars-in-rectangle.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const [n, m] = fs.readFileSync(0).toString().trim().split(" ");
+let str = "";
+
+for (let i=0; i<n; i++) {
+    str = "";
+    for (let j=0; j<m; j++) {
+        str += '*'+' ';
+    }
+    console.log(str);
+}
