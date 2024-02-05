7519fe848ae3967c8fba23c6d533e5e8ded6afa1
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-05 17:20:01 +0900

    Title: 별 그리기, Time: 335ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240205/별 그리기/star-drawing.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = "";
+
+for (let i=0; i<n; i++) {
+    str = '';
+    for (let j=0; j<n-i-1; j++) {
+        str+=' ';
+    }
+    for (let j=0; j<(2*i)+1; j++) {
+        str+='*';
+    }
+    console.log(str);
+}
+for (let i=n-2; i>=0; i--) {
+    str = '';
+    for (let j=0; j<n-i-1; j++) {
+        str+=' ';
+    }
+    for (let j=0; j<(2*i)+1; j++) {
+        str+='*';
+    }
+    console.log(str);
+}
