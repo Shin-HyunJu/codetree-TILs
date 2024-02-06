93bc66c010c9614055f105fb2fa0ab6d1c66a3d4
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-06 11:56:25 +0900

    Title: 별표 출력하기 12, Time: 242ms, Memory: 14MB, Status: Passed - CodeTree

--- 240206/별표 출력하기 12/print-star-12.js
+++ 240206/별표 출력하기 12/print-star-12.js
@@ -5,31 +5,19 @@ let str = "";
 for (let i=0; i<n; i++) {
     str = '';
 
-    if (i % 2 === 0) {
-        for (let j=0; j<i+1; j++) {
-            if (i === 0) {
-                str += '';
-            } else {
-                str += '  ';
-            }
-        }
-    } else {
-        for (let j=0; j<i; j++) {
-            str += '  ';
-        }
-    }
-
     for (let j=0; j<n; j++) {
         if (j % 2 === 0) {
             if (i === 0) {
                 str +='* ';
-            } else if (j > i) {
+            } else {
                 str += '  '
             }
         } else {
             if (i < j+1) {
                 str += '* ';
-            } 
+            } else {
+                str += '  ';
+            }
         }
     }
 
