f7d6e170ea2b58781fa3865c2e6246d649d6841b
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-06 12:09:28 +0900

    Title: 규칙을 찾아 출력하기, Time: 232ms, Memory: 14MB, Status: Passed - CodeTree

--- 240206/규칙을 찾아 출력하기/find-a-rule-and-print.js
+++ 240206/규칙을 찾아 출력하기/find-a-rule-and-print.js
@@ -6,10 +6,10 @@ for (let i = 0; i < n; i++) {
     str = '';
 
     for (let j = 0; j < n; j++){
-        if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
+        if (j < i || i === 0 || j === n - 1) {
             str += '* ';
         } else {
-            str += (j < i) ? '* ': '  ';
+            str += '  ';
         }
     }
 
