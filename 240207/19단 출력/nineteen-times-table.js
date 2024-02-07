619ce5787a909389b2120fb268cebe30cd8258e8
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-07 15:30:08 +0900

    Title: 19단 출력, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240207/19단 출력/nineteen-times-table.js
@@ -0,0 +1,11 @@
+let str = '';
+
+for (let i = 1; i <= 19; i++) {
+    for (let j = 1; j <= 19; j++) {
+        str += `${i} * ${j} = ${i * j}`;
+        if (j % 2 === 1 && j !== 19) str += ' / ';
+        else if (j% 2 === 0 || j===19) str += '\n';
+    }
+}
+
+console.log(str);
