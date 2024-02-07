1ed0da541899be4407a1934d41abcc042b123bc5
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-07 15:20:22 +0900

    Title: 특이한 출력, Time: 235ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240207/특이한 출력/special-print.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        str += `(${i}, ${j}) `;
+        if ((i + j) % 4 === 0) {
+            str += '\n';
+        }
+    }
+}
+
+console.log(str);
