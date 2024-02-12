cddc86075bf2060b0730b7ba20f8bab1fc8360d9
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-12 14:37:25 +0900

    Title: 완전수, Time: 195ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 240212/완전수/perfect-number.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const [start, end] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+let cnt = 0;
+let sum = 0;
+
+for (let i = start; i <= end; i++) {
+    sum = 0;
+    for (let j = 1; j < i; j++) {
+        if (i % j === 0) {
+            sum += j;
+        }
+    }
+    if (sum === i) {
+        cnt++;
+    }
+}
+console.log(cnt);
