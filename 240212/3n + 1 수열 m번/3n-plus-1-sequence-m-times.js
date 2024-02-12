d8ee9997fa8e8eb566841eb9d6006c5f3cbe3b32
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-12 14:25:32 +0900

    Title: 3n + 1 수열 m번, Time: 202ms, Memory: 16MB, Status: Passed - CodeTree

--- 240212/3n + 1 수열 m번/3n-plus-1-sequence-m-times.js
+++ 240212/3n + 1 수열 m번/3n-plus-1-sequence-m-times.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const n = fs.readFileSync(0).toString().trim().split('\n').map(Number);
-let count = 0;
 
 for (let i = 1; i <= n[0]; i++) {
-    count = 0;
+    let count = 0;
     let num = n[i];
     while (num !== 1) {
         if (num % 2 === 0) num /= 2;
