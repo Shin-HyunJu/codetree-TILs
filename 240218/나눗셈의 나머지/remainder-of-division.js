f7075970d92a0d07b6730ddf1a8c34de8e87f507
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-18 16:13:43 +0900

    Title: 나눗셈의 나머지, Time: 170ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240218/나눗셈의 나머지/remainder-of-division.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let [a, b] = fs.readFileSync(0).toString().trim().split(' ');
+let countArr = Array(b).fill(0);
+let sum = 0;
+
+while (a > 1) {
+    countArr[a % b];
+    a = parseInt(a / b);
+} 
+
+for (let i = 0; i < b; i++) {
+    sum += countArr[i] ** 2;
+}
+
+console.log(sum);
