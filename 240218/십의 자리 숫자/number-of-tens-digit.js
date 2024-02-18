c99729636892605412b270611c14cf5db7385e0e
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-18 15:18:11 +0900

    Title: 십의 자리 숫자, Time: 232ms, Memory: 47MB, Status: Passed - CodeTree

--- 240218/십의 자리 숫자/number-of-tens-digit.js
+++ 240218/십의 자리 숫자/number-of-tens-digit.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 let arr = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 let countArr = Array(10).fill(0);
+let i = 0;
 
-for (let i = 0; i < arr.length; i++) {
+while(arr[i] !== 0) {
     countArr[parseInt(arr[i]/10)]++;
+    i++;
 }
 
 for (let i = 1; i < 10; i++) {
