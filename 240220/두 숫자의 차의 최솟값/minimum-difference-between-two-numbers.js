327559d5972ff99b6b68e66e0cb1854e9d050cd3
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-20 20:07:32 +0900

    Title: 두 숫자의 차의 최솟값, Time: 242ms, Memory: 14MB, Status: Passed - CodeTree

--- 240220/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
+++ 240220/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -3,9 +3,9 @@ let [n, arr] = fs.readFileSync(0).toString().trim().split('\n');
 n = Number(n);
 arr = arr.split(' ').map(Number);
 
-let value = Number.MAX_SAFE_INTEGER;
+let value = arr[1] - arr[0];
 
-for (let i = 0; i < n-1; i++) {
+for (let i = 1; i < n-1; i++) {
     if (value > arr[i+1] - arr[i]) {
         value = arr[i+1] - arr[i];
     }
