600337d9945612dac05ad91d6fc6880d3ddfef32
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-02-20 20:34:40 +0900

    Title: 대문자로 바꾸기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240220/대문자로 바꾸기/change-to-capital.js
+++ 240220/대문자로 바꾸기/change-to-capital.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
-let input = fs.readFileSync(0).toString();
+let input = fs.readFileSync(0).toString().trim().split('\n');
+let diff = 'a'.charCodeAt() - 'A'.charCodeAt();
 
-input = input.toUpperCase();
-
-console.log(input);
+for (row of input) {
+    let str = '';
+    row = row.split(' ');
+    for (elem of row) {
+        let elemCode = elem.charCodeAt() - diff;
+        str += `${String.fromCharCode(elemCode)} `;
+    }
+    console.log(str);
+}
