b04975b5af7f9cc224ddf08c09c91b60aa7bce14
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-04-05 16:46:21 +0900

    Title: 배열로 사각형 만들기, Time: 232ms, Memory: 46MB, Status: Passed - CodeTree

--- 240405/배열로 사각형 만들기/print-array-in-rectangle.js
+++ 240405/배열로 사각형 만들기/print-array-in-rectangle.js
@@ -1,20 +1,14 @@
 let arr2d = Array(5).fill(0).map(() => Array(5).fill(0));
 
 for (let i = 0; i < 5; i++) {
-    arr2d[i][0] = 1;
-    arr2d[0][i] = 1;
-}
-
-for (let i = 1; i < 5; i++) {
-    for (let j = 1; j < 5; j++) {
-        arr2d[i][j] = arr2d[i-1][j] + arr2d[i][j-1];
-    }    
-}
-
-for (let i = 0; i < 5; i++) {
     let str = ''
     for (let j = 0; j < 5; j++) {
+        if (i === 0 || j === 0) {
+            arr2d[i][j] = 1;    
+        } else {
+            arr2d[i][j] = arr2d[i-1][j] + arr2d[i][j-1];
+        }
         str += arr2d[i][j] + ' ';
-    }    
-    console.log(str);
+    }
+    console.log(str);    
 }
