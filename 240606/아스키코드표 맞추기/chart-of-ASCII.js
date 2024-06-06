a787ec092b645064da8a442473e7c3be3931ff99
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-06 20:58:31 +0900

    Title: 아스키코드표 맞추기, Time: 240ms, Memory: 14MB, Status: Passed - CodeTree

--- 240606/아스키코드표 맞추기/chart-of-ASCII.js
+++ 240606/아스키코드표 맞추기/chart-of-ASCII.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let array = fs.readFileSync(0).toString().trim().split(' ');
 let str = '';
 
-for (elem of array) {
+for (let elem of array) {
     str += String.fromCharCode(Number(elem)) + ' ';
 }
 
