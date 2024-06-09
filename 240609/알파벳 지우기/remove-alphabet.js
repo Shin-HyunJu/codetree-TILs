abf78a8dd791ea85d3aed636714ef0ea11f59651
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-09 13:52:23 +0900

    Title: 알파벳 지우기, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240609/알파벳 지우기/remove-alphabet.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let sum = 0;
+
+for (let elem of input) {
+    let tempNum = '';
+    for (let val of elem) {
+        if (val >= '0' && val <= '9') {
+            tempNum += val;
+        }
+    }
+    sum += Number(tempNum);
+}
+
+console.log(sum);
