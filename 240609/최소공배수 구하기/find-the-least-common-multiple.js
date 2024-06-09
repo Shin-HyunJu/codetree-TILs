ecf227b8b7883c717ae31c1ed9b178a3b72c518b
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-09 17:32:20 +0900

    Title: 최소공배수 구하기, Time: 245ms, Memory: 14MB, Status: Passed - CodeTree

--- 240609/최소공배수 구하기/find-the-least-common-multiple.js
+++ 240609/최소공배수 구하기/find-the-least-common-multiple.js
@@ -3,14 +3,13 @@ let [num1, num2] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 // 최소공배수 = 최소공약수 x 큰 값?
 function lcd(n, m) {
-    let min = 1;
-    for (let i=2; i<=Math.min(n,m); i++) {
+    let max = 1;
+    for (let i=1; i<=Math.min(n,m); i++) {
         if (n % i === 0 && m % i === 0) {
-            min = i;
-            break;
+            max = i;
         }
     }
-    console.log(Math.max(n,m)*min);
+    console.log(n*m/max);
 }
 
 lcd(num1, num2);
