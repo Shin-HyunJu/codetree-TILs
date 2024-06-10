02935cdc8628b17cb16b8171f2e03325a50350d5
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-10 17:20:33 +0900

    Title: 함수를 이용한 369 게임, Time: 273ms, Memory: 20MB, Status: Passed - CodeTree

--- 240610/함수를 이용한 369 게임/369-games-using-functions.js
+++ 240610/함수를 이용한 369 게임/369-games-using-functions.js
@@ -3,14 +3,24 @@ let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 let count = 0;
 
 function is369Number(n) {
-    n = n.toString();
-    if((Number(n) % 3) === 0 || n.includes('3') || n.includes('6') || n.includes('9')){
-        return true;
+    // n = n.toString();
+    // if ((Number(n) % 3) === 0 || n.includes('3') || n.includes('6') || n.includes('9')) {
+    //     return true;
+    // }
+
+    if (n % 3 === 0) return true;
+
+    while (n > 0) {
+        if ( n % 10 === 3 || n % 10 === 6 || n % 10 === 9 ) {
+            return true;
+        }
+
+        n = parseInt(n / 10);
     }
 }
 
-for(let i=a; i<=b; i++) {
-    if(is369Number(i)) {
+for (let i = a; i <= b; i++) {
+    if (is369Number(i)) {
         count++;
     }
 }
