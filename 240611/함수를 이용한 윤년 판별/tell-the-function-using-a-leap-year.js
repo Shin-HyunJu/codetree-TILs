05c6e5e3a38382795aefd7e6b19fa8e295377227
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-11 12:43:38 +0900

    Title: 함수를 이용한 윤년 판별, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240611/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
+++ 240611/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 let year = Number(fs.readFileSync(0).toString().trim());
 
+// 4로 나누어 떨어지는 해는 윤년
+// 예외, 100으로 나누어 떨어지되, 400으로 나누어 떨어지지 않는 해는 평년,, 
 function isLeapYear(y) {
     if (y % 4 !== 0 ) {
         return false;
@@ -12,4 +14,21 @@ function isLeapYear(y) {
     }
 }
 
-console.log(isLeapYear(year));
+function another(y) {
+    if (y % 4 !== 0) {
+        return false;
+    }
+
+    if (y % 100 !== 0) {
+        return true;
+    }
+
+    if (y % 400 !== 0) {
+        return false;
+    }
+
+    return true;
+}
+
+
+console.log(another(year));
