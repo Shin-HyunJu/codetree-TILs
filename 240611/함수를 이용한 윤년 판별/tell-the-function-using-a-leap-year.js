493610013d066e2cfe9c65e64622a7f3f9440dbf
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-11 12:35:57 +0900

    Title: 함수를 이용한 윤년 판별, Time: 228ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240611/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let year = Number(fs.readFileSync(0).toString().trim());
+
+function isLeapYear(y) {
+    if (y % 4 !== 0 ) {
+        return false;
+    } else {
+        if (y % 100 === 0 && y % 400 !== 0) {
+            return false;
+        }
+        return true;
+    }
+}
+
+console.log(isLeapYear(year));
