c3d81b1aa2f1ab87d6c728855a483eeb8d173e63
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-11 20:24:26 +0900

    Title: 사칙연산 함수, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240611/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+let [a, o ,c] = fs.readFileSync(0).toString().trim().split(' ');
+
+function arithmetic(a, o, c) {
+    a = Number(a);
+    c = Number(c);
+
+    switch(o) {
+        case '+':
+        console.log(a, o, c, '=', a + c);
+        break;
+        case '-':
+        console.log(a, o, c, '=', a - c);
+        break;
+        case '/':
+        console.log(a, o, c, '=', parseInt(a / c));
+        break;
+        case '*':
+        console.log(a, o, c, '=', a * c);
+        break;
+    }
+}
+
+arithmetic(a, o, c);
