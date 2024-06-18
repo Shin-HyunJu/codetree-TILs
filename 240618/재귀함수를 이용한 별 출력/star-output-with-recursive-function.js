9ceca5188fd6f7636c8f639ddc72306d76216223
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-06-18 20:52:02 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 320ms, Memory: 20MB, Status: Passed - CodeTree

---
+++ 240618/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString().trim());
+
+function printStar(n) {
+    if (n === 0) {
+        return;
+    }
+
+    printStar(n-1);
+    for (let i = 0; i < n; i++) {
+        process.stdout.write('*');
+    }
+    process.stdout.write('\n');
+}
+
+printStar(input);
