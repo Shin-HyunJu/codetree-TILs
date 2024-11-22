890dae82b7e095325c549072c5002ffe98e3a3a6
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-11-22 11:08:41 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 241122/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
+++ 241122/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
@@ -2,13 +2,18 @@ const fs = require("fs");
 const n = Number(fs.readFileSync(0).toString());
 
 function printSum(n) {
-    if ( n === 1 ) {
-        return 1;
-    } else if (n === 2) {
-        return 2;
+    // if ( n === 1 ) {
+    //     return 1;
+    // } else if (n === 2) {
+    //     return 2;
+    // }
+
+    if (n < 0) {
+        return 0;
+    } else {
+        return printSum(n-2) + n;
     }
 
-    return printSum(n-2) + n;
 }
 
 console.log(printSum(n));
