7ab9d36cd108901ec0e57055c4dcf326f1dcb855
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-12-24 16:56:29 +0900

    Title: 두 개의 동일한 수열, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 241224/두 개의 동일한 수열/two-equal-series.js
+++ 241224/두 개의 동일한 수열/two-equal-series.js
@@ -3,8 +3,8 @@ let [n, a, b] = fs.readFileSync(0).toString().trim().split('\n');
 let answer = "Yes";
 
 n = Number(n);
-a = a.split(" ").map(Number).sort((a, b) => a - b);
-b = b.split(" ").map(Number).sort((a, b) => a - b);
+a = a.trim().split(" ").map(Number).sort((a, b) => a - b);
+b = b.trim().split(" ").map(Number).sort((a, b) => a - b);
 
 for(i = 0; i < n; i++) {
     if ( a[i] !== b[i] ) {
