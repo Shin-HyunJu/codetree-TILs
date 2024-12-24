5ac2b6a9a492c5898d897fc3dd6ede4e8580d6ff
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2024-12-24 16:43:33 +0900

    Title: Top k 숫자 구하기, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 241224/Top k 숫자 구하기/kth-number.js
+++ 241224/Top k 숫자 구하기/kth-number.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 let [num, list] = fs.readFileSync(0).toString().trim().split('\n');
 
 const num2 = Number(num.split(' ')[1]);
-list = list.split(' ').map(Number).sort();
+list = list.split(' ').map(Number).sort((a, b) => a - b);
+//console.table(list)
 
 console.log(list[num2 - 1])
