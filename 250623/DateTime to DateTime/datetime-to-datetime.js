f7d95502d99bec596cffad309d2faa45e9de1efd
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2025-06-23 16:01:49 +0900

    Title: DateTime to DateTime, Time: 194ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250623/DateTime to DateTime/datetime-to-datetime.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [A, B, C] = input[0].split(' ').map(Number);
+
+// Please write your code here.
+const monthDate = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function calculateMin(d, h, m) {
+    return (d - 1) * 24 * 60 + h * 60 + m;
+}
+
+console.log(calculateMin(A, B, C) - calculateMin(11, 11, 11))
