6122eddd1de33cc79a7dc7f2858d5ed4a88f9a31
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2025-07-07 12:02:17 +0900

    Title: 최대로 겹치는 구간, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250707/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const segments = [];
+for (let i = 1; i <= n; i++) {
+    segments.push(input[i].trim().split(' ').map(x => Number(x) + 100));
+}
+
+// Please write your code here.
+let array = new Array(200).fill(0);
+
+for (let i = 0; i < n; i++) {
+    for (let j = segments[i][0]; j < segments[i][1]; j++) {
+        array[j]++;
+    }    
+}
+
+console.log(Math.max(...array));
