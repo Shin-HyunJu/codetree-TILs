1021d02ac3151b216c2c465c9cb4b2d30f53c944
Shin-HyunJu <90555399+Shin-HyunJu@users.noreply.github.com>
2025-07-10 14:14:05 +0900

    Title: 왔다 갔던 구역 2, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250710/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 250710/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -4,8 +4,6 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const commands = input.slice(1, n + 1).map(x => x.split(' '));
 
-// console.log(commands);
-
 // Please Write your code here.
 let max = 0;
 let min = 0;
@@ -23,31 +21,26 @@ for (let i = 0; i < n; i++) {
 
 let array = new Array(Math.abs(min) + max + 2).fill(0);
 let newNow = Math.abs(min) + 1;
-let zero = Math.abs(min) + 1;
 let count = 0;
 
 for (let i = 0; i < n; i++) {
     if(commands[i][1] === 'R') {
-        // console.log('오른쪽', commands[i][0], 'newNow', newNow )
-
         for (let j = newNow; j < newNow + Number(commands[i][0]); j++){ // 얘는 왜 안됨?
             array[j]++;
         }
         newNow += Number(commands[i][0]);
     } else if(commands[i][1] === 'L') {
-        // console.log('왼쪽', commands[i][0], 'newNow', newNow )
-
-        for (let j = newNow; j > newNow - Number(commands[i][0]); j--){
+        for (let j = newNow - 1; j >= newNow - Number(commands[i][0]); j--){
             array[j]++;
         }
         newNow -= Number(commands[i][0]);
     }
-    // console.log(array)
+
 
 }
 
-for(let i = 0; i < n; i++) {
-    if (array[i] >= 2) {
+for(let i = 0; i < array.length; i++) {
+    if (array[i] > 1) {
         count++;
     }
 }
